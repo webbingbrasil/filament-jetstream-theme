96ff74dd6daf0c48b126c45fa07e269512b190ec
Danilo Andrade <mandrade.danilo@gmail.com>
2022-05-04 17:03:22 -0300

    fix: use filament user menu

--- resources/views/components/layouts/app/top-navigation/index.blade.php
+++ resources/views/components/layouts/app/top-navigation/index.blade.php
@@ -73,44 +73,7 @@
                 </div>
                 <!-- Settings Dropdown -->
                 <div class="ml-3 relative">
-                    <x-filament-jetstream::layouts.app.top-navigation.dropdown align="right" width="48">
-                        <x-slot name="trigger">
-                            @if ($userPhoto)
-                                <button class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition">
-                                    <img class="h-8 w-8 rounded-full object-cover" src="{{ $userPhoto }}" alt="{{ $userName }}" />
-                                </button>
-                            @else
-                                <span class="inline-flex rounded-md">
-                                    <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition">
-                                        {{ $userName }}
-
-                                        <svg class="ml-2 -mr-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                                            <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
-                                        </svg>
-                                    </button>
-                                </span>
-                            @endif
-                        </x-slot>
-
-                        <x-slot name="content">
-                            <!-- Account Management -->
-                            <div class="block px-4 py-2 text-xs text-gray-400">
-                                {{ __('Manage Account') }}
-                            </div>
-
-                            @foreach($manageAccountNavigation as $item)
-                                <x-filament-jetstream::layouts.app.top-navigation.dropdown-link href="{{ $item->getUrl() }}">
-                                    {{ $item->getLabel() }}
-                                </x-filament-jetstream::layouts.app.top-navigation.dropdown-link>
-                            @endforeach
-
-                            <div class="border-t border-gray-100"></div>
-
-                            <x-filament-jetstream::layouts.app.top-navigation.dropdown-link href="{{ route('filament.auth.logout') }}">
-                                {{ __('filament::layout.buttons.logout.label') }}
-                            </x-filament-jetstream::layouts.app.top-navigation.dropdown-link>
-                        </x-slot>
-                    </x-filament-jetstream::layouts.app.top-navigation.dropdown>
+                    <x-filament::layouts.app.topbar.user-menu />
                 </div>
             </div>
 
