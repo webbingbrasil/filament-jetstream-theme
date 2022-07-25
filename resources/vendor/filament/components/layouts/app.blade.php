700a6ab89a8bb46bc8bcbd43cd78a011032fcf4b
Danilo Andrade <mandrade.danilo@gmail.com>
2022-07-25 13:31:27 -0300

    fix: remove max content width from app layout

--- resources/vendor/filament/components/layouts/app.blade.php
+++ resources/vendor/filament/components/layouts/app.blade.php
@@ -4,24 +4,12 @@
 
 <x-filament::layouts.base :title="$title">
     <div class="min-h-screen w-full filament-app-layout">
-{{--    <div class="flex min-h-screen overflow-x-hidden w-full filament-app-layout">--}}
 
         <x-filament-jetstream::layouts.app.top-navigation />
-{{--        <x-filament::layouts.app.sidebar />--}}
 
         <div class="w-full space-y-6 flex-1 flex flex-col filament-main">
             <div @class([
                 'flex-1 w-full mx-auto filament-main-content',
-                match ($maxContentWidth ?? config('filament.layout.max_content_width')) {
-                    'xl' => 'max-w-xl',
-                    '2xl' => 'max-w-2xl',
-                    '3xl' => 'max-w-3xl',
-                    '4xl' => 'max-w-4xl',
-                    '5xl' => 'max-w-5xl',
-                    '6xl' => 'max-w-6xl',
-                    'full' => 'max-w-full',
-                    default => 'max-w-7xl',
-                },
             ])>
                 {{ \Filament\Facades\Filament::renderHook('content.start') }}
 
